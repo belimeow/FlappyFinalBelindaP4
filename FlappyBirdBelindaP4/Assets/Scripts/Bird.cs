4ae2d87055638aaa3938d00c40b6c9e1801115a9
belimeow <711592@student.dusd.net>
2022-12-20 13:50:29 -0800

    Final
    I finished it and everything went well

--- FlappyBirdBelindaP4/Assets/Scripts/Bird.cs
+++ FlappyBirdBelindaP4/Assets/Scripts/Bird.cs
@@ -33,7 +33,9 @@ public class Bird : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
